e71a3176de14fd214721d3978ea0768a6271ad3a
Angel Coco Chen <angel@nuisance.co>
2018-03-04 18:13:29 +0800

    Initial commit: Create React Native App and add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true,
+	},
+	plugins: ['eslint-plugin-react'],
+	extends: 'eslint:recommended',
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+			jsx: true,
+		},
+		sourceType: 'module',
+		allowImportExportEverywhere: false,
+		codeFrame: false,
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'react/jsx-uses-vars': 1,
+		'react/jsx-uses-react': 1,
+	},
+}
