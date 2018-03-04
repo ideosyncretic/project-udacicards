f22f46a879e26a58395852906745fd45b11d00a5
Angel Coco Chen <angel@nuisance.co>
2018-03-04 18:46:49 +0800

    :construction: chore: Add API helper functions

---
+++ utils/api.js
@@ -0,0 +1,13 @@
+import { AsyncStorage } from 'react-native'
+export const getDecks = () => {
+	return
+}
+export const getDeck = ({ id }) => {
+	return
+}
+export const saveDeckTitle = ({ title }) => {
+	return
+}
+export const addCardToDeck = ({ title, card }) => {
+	return
+}
