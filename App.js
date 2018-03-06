027cbbd3276f22bec4c9a1ce061ad544ff043d02
Angel Coco Chen <angel@nuisance.co>
2018-03-06 23:48:22 +0800

    feat: Add tab navigation and scaffold styled deck list view

--- App.js
+++ App.js
@@ -1,23 +1,91 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, View, StatusBar } from 'react-native'
+import { TabNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+import DeckList from './views/DeckList'
+import Deck from './views/Deck'
+import AddDeck from './views/AddDeck'
+import AddQuestion from './views/AddQuestion'
+import Quiz from './views/Quiz'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<StyledStatusBar backgroundColor="#000" barStyle="light-content" />
+				<Tabs />
+			</View>
+		)
+	}
+}
+
+const Tabs = TabNavigator(
+	{
+		DeckList: {
+			screen: DeckList,
+			navigationOptions: {
+				tabBarLabel: 'Decks',
+			},
+		},
+		AddDeck: {
+			screen: AddDeck,
+			navigationOptions: {
+				tabBarLabel: 'Add Decks',
+			},
+		},
+		Deck: {
+			screen: Deck,
+			navigationOptions: {
+				tabBarLabel: 'Deck',
+			},
+		},
+		Quiz: {
+			screen: Quiz,
+			navigationOptions: {
+				tabBarLabel: 'Quiz',
+			},
+		},
+		AddQuestion: {
+			screen: AddQuestion,
+			navigationOptions: {
+				tabBarLabel: 'AddQuestion',
+			},
+		},
+	},
+	{
+		navigationOptions: {
+			header: null,
+		},
+		tabBarOptions: {
+			activeTintColor: '#fff',
+			indicatorStyle: {
+				backgroundColor: '#2c3e50',
+			},
+			style: {
+				height: 56,
+				backgroundColor: 'cornflowerblue',
+				shadowColor: 'rgba(0, 0, 0, 0.24)',
+				shadowOffset: {
+					width: 0,
+					height: 3,
+				},
+				shadowRadius: 6,
+				shadowOpacity: 1,
+			},
+		},
+	},
+)
+
+const StyledStatusBar = ({ backgroundColor, ...props }) => {
+	return (
+		<View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+			<StatusBar translucent backgroundColor={backgroundColor} {...props} />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+	},
+})
--- views/DeckList.js
+++ views/DeckList.js
@@ -1,18 +1,100 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableNativeFeedback } from 'react-native'
+import styled from 'styled-components/native'
 
 // default view
 class DeckList extends Component {
+	renderDeckCard = ({ item }) => {
+		return (
+			<TouchableNativeFeedback>
+				<Card key={item.key}>
+					<Header size="h1" pb={2} primary>
+						{item.title}
+					</Header>
+					<Header size="h3">{item.cardCount} cards</Header>
+				</Card>
+			</TouchableNativeFeedback>
+		)
+	}
 	render() {
 		// TODO: display Deck titles
 		// TODO: display number of cards in each deck
 
+		const data = [
+			{
+				key: '12313',
+				title: 'Redux',
+				cardCount: 10,
+			},
+			{
+				key: '12311',
+				title: 'React',
+				cardCount: 10,
+			},
+			{
+				key: '12312',
+				title: 'React Native',
+				cardCount: 14,
+			},
+			{
+				key: '12313',
+				title: 'Web Development',
+				cardCount: 8,
+			},
+			{
+				key: '12314',
+				title: 'Coding for Designers',
+				cardCount: 10,
+			},
+			{
+				key: '12316',
+				title: 'GraphQL',
+				cardCount: 12,
+			},
+			{
+				key: '12315',
+				title: 'Server-side Rendering',
+				cardCount: 15,
+			},
+		]
 		return (
-			<View>
-				<Text>Deck List</Text>
-			</View>
+			<ViewContainer>
+				<FlatList
+					data={data}
+					renderItem={this.renderDeckCard}
+					contentContainerStyle={{ paddingTop: 16, paddingBottom: 16 }}
+				/>
+			</ViewContainer>
 		)
 	}
 }
+
+const ViewContainer = styled(View)`
+	background-color: #ecf0f1;
+`
+
+const Card = styled(View)`
+	padding: 32px 64px;
+	margin: 8px 16px;
+	align-items: center;
+	shadow-radius: 3;
+	shadow-opacity: 0.8;
+	shadow-color: rgba(0,0,0,0.2);
+	shadow-offset: {
+		width: 0,
+		height: 3,
+	};
+	background-color: white;
+	elevation: 3;
+`
+
+const Header = styled(Text)`
+	text-align: center;
+	color: ${props => (props.primary ? 'cornflowerblue' : 'grey')};
+	font-size: ${props =>
+		(props.size === 'h1' && '32px') || (props.size === 'h3' && '24px')};
+	padding: ${({ p }) => (p ? `${p * 8}px` : '0px')};
+	padding-bottom: ${({ pb }) => (pb ? `${pb * 8}px` : '0px')};
+`
 
 export default DeckList
